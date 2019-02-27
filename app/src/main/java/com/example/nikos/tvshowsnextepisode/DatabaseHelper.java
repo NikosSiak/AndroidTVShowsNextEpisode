781113e890897d4b5600289a4c25a63013e2e4b6
NikosSiak <nikos.siakas@gmail.com>
2019-02-27 15:10:50 +0200

    change in the way the show are saved. From files to database

---
+++ app/src/main/java/com/example/nikos/tvshowsnextepisode/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.nikos.tvshowsnextepisode;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+    private static final String TABLE_NAME = "shows";
+    private static final String COL0 = "link";
+    private static final String COL1 = "name";
+    private static final String COL2 = "nextEpisode";
+
+    public DatabaseHelper(Context context){
+        super(context, TABLE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL0 + " TEXT PRIMARY KEY, " +
+                COL1 + " TEXT, " +
+                COL2 + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String link, String name, String nextEpisode){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL0, link);
+        contentValues.put(COL1, name);
+        contentValues.put(COL2, nextEpisode);
+        Log.d(TAG,"addData: Adding " + name + " to " + TABLE_NAME);
+        long res = db.insert(TABLE_NAME, null, contentValues);
+
+        if (res == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void deleteShow(String link){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, COL0 + " = ?", new String[]{link});
+    }
+}
